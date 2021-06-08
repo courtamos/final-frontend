139b5485719b1182dc080cd4b871bcd942424001
Brandon Macdonald <bmacdonald1986@gmail.com>
2021-06-07 21:44:51 -0600

    Fix for sidebar and sidebar button to adhere to ESLint rules, removed blank line from App.jsx

--- src/features/common/SideBarButton.jsx
+++ src/features/common/SideBarButton.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Button
-} from '@material-ui/core';
-import { StylesProvider } from "@material-ui/core/styles";
+import { Button } from '@material-ui/core';
+import { StylesProvider } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 import './SideBarButton.scss';
 
 export const SideBarButton = (props) => {
+  const { onClick, children } = props;
 
   return (
     <StylesProvider injectFirst>
-      <Button className="button" onClick={props.onClick}>
-        { props.children }
+      <Button className="button" onClick={onClick}>
+        { children }
       </Button>
     </StylesProvider>
-  )
-}
+  );
+};
+
+SideBarButton.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+};
+
+SideBarButton.defaultProps = {
+  onClick: () => {},
+  children: {},
+};
+
+export default SideBarButton;
