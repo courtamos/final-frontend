d42f92a9cf42f2088ee52c9c73a32d6ff05974b8
Austin <austinkret@gmail.com>
2021-06-07 17:39:42 -0700

    added company logo search feature, basic styling

--- src/features/jobs/Jobs-modal.jsx
+++ src/features/jobs/Jobs-modal.jsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles, StylesProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogContent from '@material-ui/core/DialogContent';
@@ -15,6 +15,8 @@ import { useDebounce } from 'use-debounce';
 import GoogleMaps from '../components/Location-input';
 import DatePicker from '../components/DatePicker';
 import Status from '../components/Status';
+
+import './Jobs-modal.scss';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -114,55 +116,66 @@ export default function JobsModal() {
     }
   };
 
-  const debouncedText = useDebounce(company.replace(/\s/g, ''), 1000);
+  const debouncedText = useDebounce(company.replace(/\s/g, ''), 10);
+
+  const companyLogo = () => {
+    if (company.length > 0) {
+      return `//logo.clearbit.com/${debouncedText[0]}.com`;
+    }
+    return 'https://i.imgur.com/n7X5rsl.png';
+  };
 
   return (
     <div>
-      <Grid container>
-        <AddBoxIcon onClick={handleClickOpen} />
-        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} fullWidth maxWidth="sm">
-          <form onSubmit={(event) => event.preventDefault()}>
-            <DialogContent dividers>
-              <div>
-                <img src={`//logo.clearbit.com/${debouncedText[0]}.com`} alt="company logo" />
-                <TextField required id="outlined-basic" label="Company Name" variant="outlined" className={classes.textField} name="company" value={company} onChange={(event) => setCompany(event.target.value)} />
-                <Status value={status} onChange={(event) => setStatus(event.target.value)} />
-              </div>
-              <div>
-                <TextField required id="outlined-basic" label="Job Title" variant="outlined" className={classes.textField} name="title" value={title} onChange={(event) => setTitle(event.target.value)} />
-              </div>
-              <div>
-                <GoogleMaps className={classes.textField} name="location" value={location} onChange={(event) => setLocation(event.target.value)} />
-              </div>
-              <TextField id="outlined-basic" label="Job Link Url" variant="outlined" className={classes.textField} name="url" value={url} onChange={(event) => setUrl(event.target.value)} />
-              <TextField id="outlined-basic" type="number" label="Salary" variant="outlined" className={classes.textField} name="salary" value={salary} onChange={(event) => setSalary(parseInt(event.target.value, 10))} />
-              <div />
-              <div />
-              <div>
-                <TextField id="outlined-multiline-static" label="Details" multiline rows={4} variant="outlined" className={classes.textField} name="details" value={details} onChange={(event) => setDetails(event.target.value)} />
-              </div>
-              <h3 className={classes.textField}>Contact</h3>
-              <div>
-                <TextField id="outlined-basic" label="Contact Name" variant="outlined" className={classes.textField} name="contact_name" value={contact_name} onChange={(event) => setContact_name(event.target.value)} />
-                <TextField id="outlined-basic" label="Contact Email" variant="outlined" className={classes.textField} name="contact_email" value={contact_email} onChange={(event) => setContact_email(event.target.value)} />
-                <TextField id="outlined-basic" label="Contact Phone Number" variant="outlined" className={classes.textField} name="contact_phone" value={contact_phone} onChange={(event) => setContact_phone(event.target.value)} />
-                <TextField id="outlined-basic" label="Contact Links (LinkedIn)" variant="outlined" className={classes.textField} name="contact_socialmedia" value={contact_socialmedia} onChange={(event) => setContact_socialmedia(event.target.value)} />
-              </div>
-              <h3 className={classes.textField}>Events</h3>
-              <TextField id="outlined-basic" label="Upcoming Event" variant="outlined" className={classes.textField} />
-              <DatePicker />
-            </DialogContent>
-            <DialogActions>
-              <Button type="submit" autoFocus onClick={handleClose} color="primary">
-                Cancel
-              </Button>
-              <Button type="submit" onClick={() => handleSubmit()} color="primary">
-                Save changes
-              </Button>
-            </DialogActions>
-          </form>
-        </Dialog>
-      </Grid>
+      <StylesProvider>
+        <Grid container>
+          <AddBoxIcon onClick={handleClickOpen} />
+          <Dialog className="job-modal-background" onClose={handleClose} aria-labelledby="customized-dialog-title" open={open} fullWidth maxWidth="sm">
+            <form className="job-modal-box" onSubmit={(event) => event.preventDefault()}>
+              <DialogContent dividers>
+                <div>
+                  <div id="company-logo-holder">
+                    <img id="company-logo" src={companyLogo()} alt="" />
+                  </div>
+                  <TextField required id="outlined-basic" label="Company Name" variant="outlined" className={classes.textField} name="company" value={company} onChange={(event) => setCompany(event.target.value)} />
+                  <Status value={status} onChange={(event) => setStatus(event.target.value)} />
+                </div>
+                <div>
+                  <TextField required id="outlined-basic" label="Job Title" variant="outlined" className={classes.textField} name="title" value={title} onChange={(event) => setTitle(event.target.value)} />
+                </div>
+                <div>
+                  <GoogleMaps className={classes.textField} name="location" value={location} onChange={(event) => setLocation(event.target.value)} />
+                </div>
+                <TextField id="outlined-basic" label="Job Link Url" variant="outlined" className={classes.textField} name="url" value={url} onChange={(event) => setUrl(event.target.value)} />
+                <TextField id="outlined-basic" type="number" label="Salary" variant="outlined" className={classes.textField} name="salary" value={salary} onChange={(event) => setSalary(parseInt(event.target.value, 10))} />
+                <div />
+                <div />
+                <div>
+                  <TextField id="outlined-multiline-static" label="Details" multiline rows={4} variant="outlined" className={classes.textField} name="details" value={details} onChange={(event) => setDetails(event.target.value)} />
+                </div>
+                <h3 className={classes.textField}>Contact</h3>
+                <div>
+                  <TextField id="outlined-basic" label="Contact Name" variant="outlined" className={classes.textField} name="contact_name" value={contact_name} onChange={(event) => setContact_name(event.target.value)} />
+                  <TextField id="outlined-basic" label="Contact Email" variant="outlined" className={classes.textField} name="contact_email" value={contact_email} onChange={(event) => setContact_email(event.target.value)} />
+                  <TextField id="outlined-basic" label="Contact Phone Number" variant="outlined" className={classes.textField} name="contact_phone" value={contact_phone} onChange={(event) => setContact_phone(event.target.value)} />
+                  <TextField id="outlined-basic" label="Contact Links (LinkedIn)" variant="outlined" className={classes.textField} name="contact_socialmedia" value={contact_socialmedia} onChange={(event) => setContact_socialmedia(event.target.value)} />
+                </div>
+                <h3 className={classes.textField}>Events</h3>
+                <TextField id="outlined-basic" label="Upcoming Event" variant="outlined" className={classes.textField} />
+                <DatePicker />
+              </DialogContent>
+              <DialogActions>
+                <Button type="submit" autoFocus onClick={handleClose} variant="contained" color="default">
+                  Cancel
+                </Button>
+                <Button type="submit" onClick={() => handleSubmit()} variant="contained" color="primary">
+                  Save changes
+                </Button>
+              </DialogActions>
+            </form>
+          </Dialog>
+        </Grid>
+      </StylesProvider>
     </div>
   );
 }
