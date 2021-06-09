52f2b5432f6cb52b26cebe376e5aa85cc67905cb
Courtney Amos <c.amos@shaw.ca>
2021-06-08 21:19:53 -0700

    implement jobsslice with fetch jobs action and functionality in dashboard

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { Box, Container } from '@material-ui/core';
 import PropTypes from 'prop-types';
 // import axios from 'axios';
@@ -6,53 +9,82 @@ import PropTypes from 'prop-types';
 // Custom Components
 import SideBar from '../common/SideBar';
 import DashboardColumn from './DashboardColumn';
+import {
+  jobsSelector,
+  fetchJobs,
+  selectInterestedJobs,
+  selectAppliedJobs,
+  selectInterviewingJobs,
+  selectOfferJobs,
+  selectRejectedJobs,
+} from './jobs/jobsSlice';
 
 const Dashboard = () => {
   // const { data } = props;
   // Dummy Data
-  const data = {
-    interested: [{
-      company: 'Facebook',
-      title: 'Full-Stack Web Developer',
-      description: 'Remote, Full Time',
-      location: 'Calgary, AB',
-    }],
-    applied: [{
-      company: 'Facebook',
-      title: 'Full-Stack Web Developer',
-      description: 'Remote, Full Time',
-      location: 'Calgary, AB',
-    }],
-    interviewing: [{
-      company: 'Facebook',
-      title: 'Full-Stack Web Developer',
-      description: 'Remote, Full Time',
-      location: 'Calgary, AB',
-    }],
-    offer: [{
-      company: 'Facebook',
-      title: 'Full-Stack Web Developer',
-      description: 'Remote, Full Time',
-      location: 'Calgary, AB',
-    }],
-    rejected: [{
-      company: 'Facebook',
-      title: 'Full-Stack Web Developer',
-      description: 'Remote, Full Time',
-      location: 'Calgary, AB',
-    }],
-  };
+  // const data = {
+  //   interested: [{
+  //     company: 'Facebook',
+  //     title: 'Full-Stack Web Developer',
+  //     description: 'Remote, Full Time',
+  //     location: 'Calgary, AB',
+  //   }],
+  //   applied: [{
+  //     company: 'Facebook',
+  //     title: 'Full-Stack Web Developer',
+  //     description: 'Remote, Full Time',
+  //     location: 'Calgary, AB',
+  //   }],
+  //   interviewing: [{
+  //     company: 'Facebook',
+  //     title: 'Full-Stack Web Developer',
+  //     description: 'Remote, Full Time',
+  //     location: 'Calgary, AB',
+  //   }],
+  //   offer: [{
+  //     company: 'Facebook',
+  //     title: 'Full-Stack Web Developer',
+  //     description: 'Remote, Full Time',
+  //     location: 'Calgary, AB',
+  //   }],
+  //   rejected: [{
+  //     company: 'Facebook',
+  //     title: 'Full-Stack Web Developer',
+  //     description: 'Remote, Full Time',
+  //     location: 'Calgary, AB',
+  //   }],
+  // };
+
+  const dispatch = useDispatch();
+  const { status } = useSelector(jobsSelector);
+  const interestedJobs = useSelector(selectInterestedJobs);
+  const appliedJobs = useSelector(selectAppliedJobs);
+  const interviewingJobs = useSelector(selectInterviewingJobs);
+  const offerJobs = useSelector(selectOfferJobs);
+  const rejectedJobs = useSelector(selectRejectedJobs);
+
+  useEffect(() => {
+    dispatch(fetchJobs());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return (
+      <Backdrop open>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  }
 
   return (
     <Container id="dashboard-container" disableGutters style={{ margin: '0px' }}>
       <Box display="flex" flexDirection="row" width="100vw">
         <SideBar />
         <Box id="dashboard-columns" display="flex" flexGrow={1} justifyContent="space-between">
-          <DashboardColumn items={data.interested} title="Interested" color="#ffe7d6" />
-          <DashboardColumn items={data.applied} title="Applied" color="#ffa7a5" />
-          <DashboardColumn items={data.interviewing} title="Interviewing" color="#ee6a7c" />
-          <DashboardColumn items={data.offer} title="Offer" color="#ab5675" />
-          <DashboardColumn items={data.rejected} title="Rejected" color="#73464f" />
+          <DashboardColumn items={interestedJobs} title="Interested" color="#ffe7d6" />
+          <DashboardColumn items={appliedJobs} title="Applied" color="#ffa7a5" />
+          <DashboardColumn items={interviewingJobs} title="Interviewing" color="#ee6a7c" />
+          <DashboardColumn items={offerJobs} title="Offer" color="#ab5675" />
+          <DashboardColumn items={rejectedJobs} title="Rejected" color="#73464f" />
         </Box>
       </Box>
     </Container>
