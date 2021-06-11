bb87fc401b515f551d70aa2e07963ca7192a17a1
Courtney Amos <c.amos@shaw.ca>
2021-06-10 21:24:38 -0700

    remove extra indentations in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import App from './App';
 import store from './app/store';
 
 ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
